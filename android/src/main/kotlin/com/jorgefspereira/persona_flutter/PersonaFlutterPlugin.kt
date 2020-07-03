6d8bdbb4e06bc5971e5dd0d149a7b3bd6fb9de85
Jorge Pereira <jorgefspereira@icloud.com>
2020-07-03 16:57:30 +0100

    bug fix android handlers not completed

--- android/src/main/kotlin/com/jorgefspereira/persona_flutter/PersonaFlutterPlugin.kt
+++ android/src/main/kotlin/com/jorgefspereira/persona_flutter/PersonaFlutterPlugin.kt
@@ -3,6 +3,10 @@ package com.jorgefspereira.persona_flutter
 import android.app.Activity
 import android.content.Intent
 import androidx.annotation.NonNull;
+import com.withpersona.sdk.inquiry.Inquiry
+import com.withpersona.sdk.inquiry.Attributes
+import com.withpersona.sdk.inquiry.Relationships
+import com.withpersona.sdk.inquiry.Verification
 import com.withpersona.sdk.inquiry.Environment
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -12,15 +16,21 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-import com.withpersona.sdk.inquiry.Inquiry
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.PluginRegistry
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 /** PersonaFlutterPlugin */
 public class PersonaFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.ActivityResultListener {
   private lateinit var channel : MethodChannel
   private var activity: Activity? = null
+  private var binding: ActivityPluginBinding? = null
   private val requestCode = 57
 
   companion object {
@@ -29,6 +39,7 @@ public class PersonaFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAwa
       val channel = MethodChannel(registrar.messenger(), "persona_flutter")
       val plugin = PersonaFlutterPlugin()
       plugin.activity = registrar.activity()
+      registrar.addActivityResultListener(plugin);
       channel.setMethodCallHandler(plugin)
     }
   }
@@ -85,15 +96,18 @@ public class PersonaFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAwa
       when(val result = Inquiry.onActivityResult(data)) {
         is Inquiry.Response.Success -> {
           val arguments = hashMapOf<String, Any?>();
-          arguments["attributes"] = "";
-          arguments["relationships"] = "";
+          arguments["inquiryId"] = result.inquiryId;
+          arguments["attributes"] = attributesToMap(result.attributes);
+          arguments["relationships"] = relationshipsToArray(result.relationships);
+
           channel.invokeMethod("onSuccess", arguments);
           return true;
         }
         is Inquiry.Response.Failure -> {
           val arguments = hashMapOf<String, Any?>();
-          arguments["attributes"] = "";
-          arguments["relationships"] = "";
+          arguments["inquiryId"] = result.inquiryId;
+          arguments["attributes"] = attributesToMap(result.attributes);
+          arguments["relationships"] = relationshipsToArray(result.relationships);
           channel.invokeMethod("onFailed", arguments);
           return true;
         }
@@ -113,22 +127,63 @@ public class PersonaFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAwa
     return false;
   }
 
+  /// - Helpers
+
+  private fun attributesToMap(attributes: Attributes): HashMap<String, Any?> {
+    val map = hashMapOf<String, Any?>();
+
+    map["firstName"] = attributes.name?.first;
+    map["middleName"] = attributes.name?.middle;
+    map["lastName"] = attributes.name?.last;
+    map["street1"] = attributes.address?.street1;
+    map["street2"] = attributes.address?.street2;
+    map["city"] = attributes.address?.city;
+    map["subdivision"] = attributes.address?.subdivision;
+    map["postalCode"] = attributes.address?.postalCode;
+    map["countryCode"] = attributes.address?.countryCode;
+
+    if (attributes.birthdate is Date) {
+      val formatter = SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+      formatter.format(attributes.birthdate);
+      map["birthdate"] = formatter.format(attributes.birthdate);
+    }
+
+    return map;
+  }
+
+  private fun relationshipsToArray(relationships: Relationships): ArrayList<HashMap<String, Any?>> {
+    val list = arrayListOf<HashMap<String,Any?>>();
+
+    for (verification in relationships.verifications) {
+      val item = hashMapOf<String, Any?>();
+      item["id"] = verification.id;
+      item["status"] = verification.status.name;
+      list.add(item);
+    }
+
+    return list;
+  }
+
   /// - ActivityAware interface
 
   override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    this.activity = binding.activity
+    this.binding = binding;
+    this.activity = binding.activity;
+    binding.addActivityResultListener(this);
   }
 
   override fun onDetachedFromActivity() {
-    this.activity = null
+    this.binding?.removeActivityResultListener(this);
+    this.activity = null;
+    this.binding = null;
   }
 
   override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    this.activity = binding.activity
+    onAttachedToActivity(binding);
   }
 
   override fun onDetachedFromActivityForConfigChanges() {
-    this.activity = null
+    onDetachedFromActivity();
   }
 
 }
