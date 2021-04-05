c5608573c4d184bfbcf4d01886715289e67aa965
Jorge Pereira <jorgefspereira@icloud.com>
2021-04-05 19:02:28 +0100

    flutter format

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,7 +24,8 @@ class _MyAppState extends State<MyApp> {
         name: InquiryName(first: "John", middle: "Apple", last: "Seed"),
         additionalFields: {"test-1": "test-2", "test-3": 2, "test-4": true},
       ),
-      onSuccess: (String inquiryId, InquiryAttributes attributes, InquiryRelationships relationships) {
+      onSuccess: (String inquiryId, InquiryAttributes attributes,
+          InquiryRelationships relationships) {
         print("onSuccess");
         print("- inquiryId: $inquiryId");
         print("- attributes:");
@@ -37,7 +38,8 @@ class _MyAppState extends State<MyApp> {
         print("--- address_postalCode: ${attributes.address.postalCode}");
         print("--- address_countryCode: ${attributes.address.countryCode}");
         print("--- address_subdivision: ${attributes.address.subdivision}");
-        print("--- address_subdivisionAbbr: ${attributes.address.subdivisionAbbr}");
+        print(
+            "--- address_subdivisionAbbr: ${attributes.address.subdivisionAbbr}");
         print("--- birthdate: ${attributes.birthdate.toString()}");
         print("- relationships:");
 
@@ -58,7 +60,8 @@ class _MyAppState extends State<MyApp> {
       onCancelled: () {
         print("onCancelled");
       },
-      onFailed: (String inquiryId, InquiryAttributes attributes, InquiryRelationships relationships) {
+      onFailed: (String inquiryId, InquiryAttributes attributes,
+          InquiryRelationships relationships) {
         print("onFailed");
         print("- inquiryId: $inquiryId");
       },
