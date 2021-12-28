698a5d1b4e07187e2b0e0934ebccbc61ffa68600
Jacky <jacky@dtravel.com>
2021-12-28 14:20:21 +0700

    migrate android impl to sdk v2

--- android/src/main/kotlin/com/jorgefspereira/persona_flutter/PersonaFlutterPlugin.kt
+++ android/src/main/kotlin/com/jorgefspereira/persona_flutter/PersonaFlutterPlugin.kt
@@ -16,9 +16,6 @@ import io.flutter.plugin.common.PluginRegistry.Registrar
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.PluginRegistry
-import java.text.SimpleDateFormat
-import java.util.Date
-import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
 /** PersonaFlutterPlugin */
@@ -60,11 +57,10 @@ public class PersonaFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAwa
         val accountId = call.argument<String>("accountId")
         val environment = call.argument<String>("environment")
         val fieldsMap = call.argument<Map<String, Any>>("fields")
-        val note = call.argument<String>("note")
 
         if (inquiryId != null) {
           Inquiry.fromInquiry(inquiryId)
-                  .accessToken(accessToken)
+                  .sessionToken(accessToken)
                   .build().start(activity, requestCode);
 
           result.success("Inquiry started with inquiryId")
@@ -86,7 +82,12 @@ public class PersonaFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAwa
               val middle = nameMap["middle"] as String?
               val last = nameMap["last"] as String?
 
-              fieldsBuilder.name(first, middle, last)
+              if (first != null)
+               fieldsBuilder.field("nameFirst", first)
+              if (middle != null)
+                fieldsBuilder.field("nameMiddle", middle)
+              if (last != null)
+                fieldsBuilder.field("nameLast", last)
             }
 
             if(addressMap != null ) {
@@ -97,21 +98,30 @@ public class PersonaFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAwa
               val postalCode = addressMap["postalCode"] as? String
               val countryCode = addressMap["countryCode"] as? String
 
-              fieldsBuilder.address(street1, street2, city, subdivision, postalCode, countryCode)
+              if (street1 != null)
+                fieldsBuilder.field("addressStreet1", street1)
+              if (street2 != null)
+                fieldsBuilder.field("addressStreet2", street2)
+              if (city != null)
+                fieldsBuilder.field("addressCity", city)
+              if (subdivision != null)
+                fieldsBuilder.field("addressSubdivision", subdivision)
+              if (postalCode != null)
+                fieldsBuilder.field("addressPostalCode", postalCode)
+              if (countryCode != null)
+                fieldsBuilder.field("addressCountryCode", countryCode)
             }
 
             if (emailAddress != null) {
-              fieldsBuilder.emailAddress(emailAddress)
+              fieldsBuilder.field("emailAddress", emailAddress)
             }
 
             if (phoneNumber != null) {
-              fieldsBuilder.phoneNumber(phoneNumber)
+              fieldsBuilder.field("phoneNumber", phoneNumber)
             }
 
             if(birthdate != null) {
-              val formatter = SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
-              formatter.format(birthdate);
-              fieldsBuilder.birthdate(formatter.calendar)
+              fieldsBuilder.field("birthDate", birthdate);
             }
 
             if(additionalFields != null) {
@@ -129,7 +139,6 @@ public class PersonaFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAwa
                   .accountId(accountId)
                   .referenceId(referenceId)
                   .fields(fieldsBuilder.build())
-                  .note(note)
 
           if (environment != null) {
             inquiry.environment(Environment.valueOf(environment.toUpperCase()));
@@ -148,115 +157,79 @@ public class PersonaFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAwa
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
     if (requestCode == requestCode) {
       when(val result = Inquiry.onActivityResult(data)) {
-        is Inquiry.Response.Success -> {
-          val arguments = hashMapOf<String, Any?>();
-          arguments["inquiryId"] = result.inquiryId;
-          arguments["attributes"] = attributesToMap(result.attributes);
-          arguments["relationships"] = relationshipsToArrayMap(result.relationships);
-
-          channel.invokeMethod("onSuccess", arguments);
-          return true;
+        is InquiryResponse.Complete -> {
+          val arguments = hashMapOf<String, Any?>()
+          arguments["inquiryId"] = result.inquiryId
+          arguments["attributes"] = fieldsToMap(result.fields)
+          if (result.status == "completed") {
+            channel.invokeMethod("onSuccess", arguments)
+          } else if (result.status == "failed") {
+            channel.invokeMethod("onFailed", arguments)
+          }
+          return true
         }
-        is Inquiry.Response.Failure -> {
-          val arguments = hashMapOf<String, Any?>();
-          arguments["inquiryId"] = result.inquiryId;
-          arguments["attributes"] = attributesToMap(result.attributes);
-          arguments["relationships"] = relationshipsToArrayMap(result.relationships);
-          channel.invokeMethod("onFailed", arguments);
-          return true;
+        is InquiryResponse.Cancel -> {
+          channel.invokeMethod("onCancelled", null)
+          return true
         }
-        Inquiry.Response.Cancel -> {
-          channel.invokeMethod("onCancelled", null);
-          return true;
-        }
-        is Inquiry.Response.Error -> {
-          val arguments = hashMapOf<String, Any?>();
-          arguments["error"] = result.debugMessage;
-          channel.invokeMethod("onError", arguments);
-          return true;
+        is InquiryResponse.Error -> {
+          val arguments = hashMapOf<String, Any?>()
+          arguments["error"] = result.debugMessage
+          channel.invokeMethod("onError", arguments)
+          return true
         }
       }
     }
 
-    return false;
+    return false
   }
 
   /// - Helpers
 
-  private fun attributesToMap(attributes: Attributes): HashMap<String, Any?> {
+  private fun fieldsToMap(fields: Map<String, InquiryField>): HashMap<String, Any?> {
     val result = hashMapOf<String, Any?>();
     val nameMap = hashMapOf<String, Any?>();
     val addressMap = hashMapOf<String, Any?>();
 
-    nameMap["first"] = attributes.name?.first;
-    nameMap["middle"] = attributes.name?.middle;
-    nameMap["last"] = attributes.name?.last;
+    nameMap["first"] = (fields["nameFirst"] as? InquiryField.StringField)?.value
+    nameMap["middle"] = (fields["nameMiddle"] as? InquiryField.StringField)?.value
+    nameMap["last"] = fields["nameLast"];
 
-    addressMap["street1"] = attributes.address?.street1;
-    addressMap["street2"] = attributes.address?.street2;
-    addressMap["city"] = attributes.address?.city;
-    addressMap["subdivision"] = attributes.address?.subdivision;
-    addressMap["subdivisionAbbr"] = attributes.address?.subdivisionAbbr;
-    addressMap["postalCode"] = attributes.address?.postalCode;
-    addressMap["countryCode"] = attributes.address?.countryCode;
+    addressMap["street1"] = (fields["addressStreet1"] as? InquiryField.StringField)?.value
+    addressMap["street2"] = (fields["addressStreet2"] as? InquiryField.StringField)?.value
+    addressMap["city"] = (fields["addressCity"] as? InquiryField.StringField)?.value
+    addressMap["subdivision"] = (fields["addressSubdivision"] as? InquiryField.StringField)?.value
+    addressMap["subdivisionAbbr"] = (fields["addressSubdivisionAbbr"] as? InquiryField.StringField)?.value
+    addressMap["postalCode"] = (fields["addressPostalCode"] as? InquiryField.StringField)?.value
+    addressMap["countryCode"] = (fields["addressCountryCode"] as? InquiryField.StringField)?.value
 
-    result["name"] = nameMap;
-    result["address"] = addressMap;
+    result["name"] = nameMap
+    result["address"] = addressMap
 
-    if (attributes.birthdate is Date) {
-      val formatter = SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-      result["birthdate"] = formatter.format(attributes.birthdate);
-    }
+    result["birthdate"] = (fields["birthdate"] as? InquiryField.StringField)?.value
 
-    return result;
-  }
-
-  private fun relationshipsToArrayMap(relationships: Relationships): ArrayList<HashMap<String, Any?>> {
-    val list = arrayListOf<HashMap<String,Any?>>();
-
-    for (verification in relationships.verifications) {
-      val item = hashMapOf<String, Any?>();
-      item["id"] = verification.id;
-
-      when (verification.status) {
-        Verification.Status.PASSED -> item["status"] = "passed"
-        Verification.Status.FAILED -> item["status"] = "failed"
-        Verification.Status.REQUIRES_RETRY -> item["status"] = "requiresRetry"
-      }
-
-      when (verification) {
-        is Verification.GovernmentId -> item["type"] = "governmentId"
-        is Verification.Database -> item["type"] = "database"
-        is Verification.PhoneNumber -> item["type"] = "phoneNumber"
-        is Verification.Document -> item["type"] = "document"
-        is Verification.Selfie -> item["type"] = "selfie"
-      }
-
-      list.add(item);
-    }
-
-    return list;
+    return result
   }
 
   /// - ActivityAware interface
 
   override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    this.binding = binding;
-    this.activity = binding.activity;
-    binding.addActivityResultListener(this);
+    this.binding = binding
+    this.activity = binding.activity
+    binding.addActivityResultListener(this)
   }
 
   override fun onDetachedFromActivity() {
-    this.binding?.removeActivityResultListener(this);
-    this.activity = null;
-    this.binding = null;
+    this.binding?.removeActivityResultListener(this)
+    this.activity = null
+    this.binding = null
   }
 
   override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    onAttachedToActivity(binding);
+    onAttachedToActivity(binding)
   }
 
   override fun onDetachedFromActivityForConfigChanges() {
-    onDetachedFromActivity();
+    onDetachedFromActivity()
   }
 }
