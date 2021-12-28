eaf26bb96b298fe606f8287955d27fb4213458bf
Jacky <jacky@dtravel.com>
2021-12-28 22:26:18 +0700

    migrate android impl to sdk v2

--- android/src/main/kotlin/com/jorgefspereira/persona_flutter/PersonaFlutterPlugin.kt
+++ android/src/main/kotlin/com/jorgefspereira/persona_flutter/PersonaFlutterPlugin.kt
@@ -16,220 +16,207 @@ import io.flutter.plugin.common.PluginRegistry.Registrar
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.PluginRegistry
+import java.util.*
 import kotlin.collections.HashMap
 
 /** PersonaFlutterPlugin */
-public class PersonaFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.ActivityResultListener {
-  private lateinit var channel : MethodChannel
-  private var activity: Activity? = null
-  private var binding: ActivityPluginBinding? = null
-  private val requestCode = 57
+public class PersonaFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.ActivityResultListener {
+    private lateinit var channel: MethodChannel
+    private var activity: Activity? = null
+    private var binding: ActivityPluginBinding? = null
+    private val requestCode = 57
+    private var inquiry: Inquiry? = null
 
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "persona_flutter")
-      val plugin = PersonaFlutterPlugin()
-      plugin.activity = registrar.activity()
-      registrar.addActivityResultListener(plugin);
-      channel.setMethodCallHandler(plugin)
-    }
-  }
-
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "persona_flutter")
-    channel.setMethodCallHandler(this);
-  }
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    when (call.method) {
-      "start" -> {
-        val activity = this.activity ?: return
-
-        val inquiryId = call.argument<String>("inquiryId")
-        val accessToken = call.argument<String>("accessToken")
-        val templateId = call.argument<String>("templateId")
-        val referenceId = call.argument<String>("referenceId")
-        val accountId = call.argument<String>("accountId")
-        val environment = call.argument<String>("environment")
-        val fieldsMap = call.argument<Map<String, Any>>("fields")
-
-        if (inquiryId != null) {
-          Inquiry.fromInquiry(inquiryId)
-                  .sessionToken(accessToken)
-                  .build().start(activity, requestCode);
-
-          result.success("Inquiry started with inquiryId")
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val channel = MethodChannel(registrar.messenger(), "persona_flutter")
+            val plugin = PersonaFlutterPlugin()
+            plugin.activity = registrar.activity()
+            registrar.addActivityResultListener(plugin);
+            channel.setMethodCallHandler(plugin)
         }
-        else if(templateId != null) {
+    }
 
-          val fieldsBuilder = Fields.Builder()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "persona_flutter")
+        channel.setMethodCallHandler(this);
+    }
 
-          if (fieldsMap != null) {
-            val nameMap = fieldsMap["name"] as? Map<*, *>
-            val addressMap = fieldsMap["address"] as? Map<*, *>
-            val emailAddress = fieldsMap["emailAddress"] as? String
-            val phoneNumber = fieldsMap["phoneNumber"] as? String
-            val birthdate = fieldsMap["birthdate"] as? String
-            val additionalFields = fieldsMap["additionalFields"] as? Map<String, *>
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 
-            if(nameMap != null) {
-              val first = nameMap["first"] as String?
-              val middle = nameMap["middle"] as String?
-              val last = nameMap["last"] as String?
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
+            "init" -> {
+                val arguments = call.arguments as? Map<String, Any?> ?: return
 
-              if (first != null)
-               fieldsBuilder.field("nameFirst", first)
-              if (middle != null)
-                fieldsBuilder.field("nameMiddle", middle)
-              if (last != null)
-                fieldsBuilder.field("nameLast", last)
-            }
+                // Fields
+                var fields: Fields? = null
 
-            if(addressMap != null ) {
-              val street1 = addressMap["street1"] as? String
-              val street2 = addressMap["street2"] as? String
-              val city = addressMap["city"] as? String
-              val subdivision = addressMap["subdivision"] as? String
-              val postalCode = addressMap["postalCode"] as? String
-              val countryCode = addressMap["countryCode"] as? String
-
-              if (street1 != null)
-                fieldsBuilder.field("addressStreet1", street1)
-              if (street2 != null)
-                fieldsBuilder.field("addressStreet2", street2)
-              if (city != null)
-                fieldsBuilder.field("addressCity", city)
-              if (subdivision != null)
-                fieldsBuilder.field("addressSubdivision", subdivision)
-              if (postalCode != null)
-                fieldsBuilder.field("addressPostalCode", postalCode)
-              if (countryCode != null)
-                fieldsBuilder.field("addressCountryCode", countryCode)
-            }
-
-            if (emailAddress != null) {
-              fieldsBuilder.field("emailAddress", emailAddress)
-            }
-
-            if (phoneNumber != null) {
-              fieldsBuilder.field("phoneNumber", phoneNumber)
-            }
-
-            if(birthdate != null) {
-              fieldsBuilder.field("birthDate", birthdate);
-            }
-
-            if(additionalFields != null) {
-              for ((key, value) in additionalFields) {
-                when(value) {
-                  is Int -> fieldsBuilder.field(key, value)
-                  is String -> fieldsBuilder.field(key, value)
-                  is Boolean -> fieldsBuilder.field(key, value)
+                (arguments["fields"] as?  Map<String, Any?>)?.let  {
+                    fields = fieldsFromMap(it)
                 }
-              }
+
+                // Configuration
+                (arguments["inquiryId"] as? String)?.let {
+                    inquiry = Inquiry
+                            .fromInquiry(it)
+                            .sessionToken(arguments["sessionToken"] as? String).build()
+                } ?: run {
+                    var environment: Environment? = null
+
+                    // Environment
+                    (arguments["environment"] as? String)?.let {
+                        environment = Environment.valueOf(it.toUpperCase(Locale.getDefault()))
+                    }
+
+                    var builder: InquiryTemplateBuilder? = null
+
+                    // Fields
+                    (arguments["templateVersion"] as? String)?.let { templateVersion ->
+                        (arguments["accountId"] as? String)?.let { accountId ->
+                            builder = Inquiry
+                                    .fromTemplateVersion(templateVersion)
+                                    .accountId(accountId)
+                        } ?: (arguments["referenceId"] as? String)?.let { referenceId ->
+                            builder = Inquiry
+                                    .fromTemplateVersion(templateVersion)
+                                    .referenceId(referenceId)
+                        } ?: run {
+                            builder = Inquiry.fromTemplateVersion(templateVersion)
+                        }
+                    } ?: (arguments["templateId"] as? String)?.let { templateId ->
+                        (arguments["accountId"] as? String)?.let { accountId ->
+                            builder = Inquiry
+                                    .fromTemplate(templateId)
+                                    .accountId(accountId)
+                        } ?: (arguments["referenceId"] as? String)?.let { referenceId ->
+                            builder = Inquiry
+                                    .fromTemplate(templateId)
+                                    .referenceId(referenceId)
+                        } ?: run {
+                            builder = Inquiry.fromTemplate(templateId)
+                        }
+                    }
+
+                    environment?.let {
+                        builder = builder?.environment(it)
+                    }
+                    fields?.let {
+                        builder = builder?.fields(it)
+                    }
+
+                    inquiry = builder?.build()
+                }
             }
-          }
+            "start" -> {
+                val activity = this.activity ?: return
+                val inquiry = this.inquiry ?: return
 
-          val inquiry = Inquiry.fromTemplate(templateId)
-                  .accountId(accountId)
-                  .referenceId(referenceId)
-                  .fields(fieldsBuilder.build())
-
-          if (environment != null) {
-            inquiry.environment(Environment.valueOf(environment.toUpperCase()));
-          }
-
-          inquiry.build().start(activity, requestCode)
-          result.success("Inquiry started with templateId")
+                inquiry.start(activity, requestCode)
+                result.success("Inquiry started with templateId")
+            }
+            else -> result.notImplemented()
         }
-      }
-      else -> result.notImplemented()
-    }
-  }
-
-  /// - ActivityResultListener interface
-
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
-    if (requestCode == requestCode) {
-      when(val result = Inquiry.onActivityResult(data)) {
-        is InquiryResponse.Complete -> {
-          val arguments = hashMapOf<String, Any?>()
-          arguments["inquiryId"] = result.inquiryId
-          arguments["attributes"] = fieldsToMap(result.fields)
-          if (result.status == "completed") {
-            channel.invokeMethod("onSuccess", arguments)
-          } else if (result.status == "failed") {
-            channel.invokeMethod("onFailed", arguments)
-          }
-          return true
-        }
-        is InquiryResponse.Cancel -> {
-          channel.invokeMethod("onCancelled", null)
-          return true
-        }
-        is InquiryResponse.Error -> {
-          val arguments = hashMapOf<String, Any?>()
-          arguments["error"] = result.debugMessage
-          channel.invokeMethod("onError", arguments)
-          return true
-        }
-      }
     }
 
-    return false
-  }
+    /// - ActivityResultListener interface
 
-  /// - Helpers
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
+        if (requestCode == requestCode) {
+            when (val result = Inquiry.onActivityResult(data)) {
+                is InquiryResponse.Complete -> {
+                    val arguments = hashMapOf<String, Any?>()
+                    arguments["inquiryId"] = result.inquiryId
+                    arguments["status"] = result.status
+                    arguments["fields"] = fieldsToMap(result.fields)
+                    channel.invokeMethod("onComplete", arguments)
+                    return true
+                }
+                is InquiryResponse.Cancel -> {
+                    val arguments = hashMapOf<String, Any?>()
+                    arguments["inquiryId"] = result.inquiryId
+                    arguments["sessionToken"] = result.sessionToken
+                    channel.invokeMethod("onCanceled", arguments)
+                    return true
+                }
+                is InquiryResponse.Error -> {
+                    val arguments = hashMapOf<String, Any?>()
+                    arguments["error"] = result.debugMessage
+                    channel.invokeMethod("onError", arguments)
+                    return true
+                }
+            }
+        }
 
-  private fun fieldsToMap(fields: Map<String, InquiryField>): HashMap<String, Any?> {
-    val result = hashMapOf<String, Any?>();
-    val nameMap = hashMapOf<String, Any?>();
-    val addressMap = hashMapOf<String, Any?>();
+        return false
+    }
 
-    nameMap["first"] = (fields["nameFirst"] as? InquiryField.StringField)?.value
-    nameMap["middle"] = (fields["nameMiddle"] as? InquiryField.StringField)?.value
-    nameMap["last"] = fields["nameLast"];
+    /// - Helpers
 
-    addressMap["street1"] = (fields["addressStreet1"] as? InquiryField.StringField)?.value
-    addressMap["street2"] = (fields["addressStreet2"] as? InquiryField.StringField)?.value
-    addressMap["city"] = (fields["addressCity"] as? InquiryField.StringField)?.value
-    addressMap["subdivision"] = (fields["addressSubdivision"] as? InquiryField.StringField)?.value
-    addressMap["subdivisionAbbr"] = (fields["addressSubdivisionAbbr"] as? InquiryField.StringField)?.value
-    addressMap["postalCode"] = (fields["addressPostalCode"] as? InquiryField.StringField)?.value
-    addressMap["countryCode"] = (fields["addressCountryCode"] as? InquiryField.StringField)?.value
+    private fun fieldsToMap(fields: Map<String, InquiryField>): HashMap<String, Any?> {
+        val result = hashMapOf<String, Any?>()
 
-    result["name"] = nameMap
-    result["address"] = addressMap
+        for ((key, value) in fields) {
+            when (value) {
+                is InquiryField.StringField -> {
+                    result[key] = value.value
+                }
+                is InquiryField.BooleanField -> {
+                    result[key] = value.value
+                }
+                is InquiryField.IntegerField -> {
+                    result[key] = value.value
+                }
+                else -> {
 
-    result["birthdate"] = (fields["birthdate"] as? InquiryField.StringField)?.value
+                }
+            }
+        }
 
-    return result
-  }
+        return result
+    }
 
-  /// - ActivityAware interface
+    private fun fieldsFromMap(map: Map<String, Any?>): Fields {
+        val result = Fields.Builder()
+        for ((key, value) in map) {
+            when (value) {
+                is String -> {
+                    result.field(key, value)
+                }
+                is Boolean -> {
+                    result.field(key, value)
+                }
+                is Int -> {
+                    result.field(key, value)
+                }
+            }
+        }
+        return result.build()
+    }
 
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    this.binding = binding
-    this.activity = binding.activity
-    binding.addActivityResultListener(this)
-  }
+    /// - ActivityAware interface
 
-  override fun onDetachedFromActivity() {
-    this.binding?.removeActivityResultListener(this)
-    this.activity = null
-    this.binding = null
-  }
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        this.binding = binding
+        this.activity = binding.activity
+        binding.addActivityResultListener(this)
+    }
 
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    onAttachedToActivity(binding)
-  }
+    override fun onDetachedFromActivity() {
+        this.binding?.removeActivityResultListener(this)
+        this.activity = null
+        this.binding = null
+        this.inquiry = null
+    }
 
-  override fun onDetachedFromActivityForConfigChanges() {
-    onDetachedFromActivity()
-  }
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        onAttachedToActivity(binding)
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity()
+    }
 }
