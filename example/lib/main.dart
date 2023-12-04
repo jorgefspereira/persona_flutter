3a18995d898010747356e0e0e4f62f34c6076699
c_weerasinghe <chanaka.weerasinghe@hitachi-dps.com>
2023-12-04 16:12:49 +0800

    Change example main file to reflect environmentID

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,6 +25,7 @@ class _MyAppState extends State<MyApp> {
 
     _configuration = TemplateIdConfiguration(
       templateId: "TEMPLATE_ID",
+      environmentId: "ENVIRONMENT_ID", // optional
       environment: InquiryEnvironment.sandbox,
       theme: InquiryTheme(
         source: InquiryThemeSource.client,
